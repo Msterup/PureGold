60c16123f5161e268e9e5c4f8dc03caff3b9b47a
Msterup <mikkel.sterup@gmail.com>
2022-03-19 23:49:27 +0100

    back2cpu for sentnet

--- precalc/main_pre.py
+++ precalc/main_pre.py
@@ -95,8 +95,7 @@ def gameloop():
     while True:
         e += 1
         state_dict = r.get('state_dict')
-        reg_agent = torch.load(io.BytesIO(agent_obj), map_location=torch.device('cpu'))
-        reg_agent.to_cuda()
+        reg_agent = torch.load(state_dict, map_location=torch.device('cpu'))
         print(f"Hur rate is: {reg_agent.nik_rate}")
 
         time_list = 53 * [0]

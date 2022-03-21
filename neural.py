79467042b1f16ac0192f9af1e93893df08cede0e
Msterup <mikkel.sterup@gmail.com>
2022-03-21 01:40:01 +0100

    small net test

--- neural.py
+++ neural.py
@@ -39,11 +39,9 @@ class ResidualBlock(nn.Module):
 class ResNet(nn.Module):
     def __init__(self, block, layers, in_channels=378, out_channels=378):
         super(ResNet, self).__init__()
-        self.fc1 = nn.Linear(in_channels, out_channels)
-        self.relu = nn.ReLU()
-        self.layer1 = self.make_layer(block, layers)
-        #self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(in_channels, 1)
+        self.fc1 = nn.Linear(378, 1000)  # 5*5 from image dimension
+        self.fc2 = nn.Linear(1000, 1000)
+        self.fc3 = nn.Linear(1000, 1)
         self.relu = nn.ReLU()
 
 
@@ -56,6 +54,7 @@ class ResNet(nn.Module):
     def forward(self, x):
         out = self.fc1(x)
         out = self.relu(out)
-        out = self.layer1(out)
-        out = self.fc2(out)
+        out = self.fc2(x)
+        out = self.relu(out)
+        out = self.fc3(out)
         return out
